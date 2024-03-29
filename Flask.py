d0032bd034abe524ffefc55377ae5634161ee635
mr-verrma <ak.amy00@gmail.com>
2024-03-29 22:02:41 +0530

    added development server

--- Flask.py
+++ Flask.py
@@ -12,4 +12,5 @@ def get_products():
     return "API FETCH CALL"
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000)
+    # app.run(host='0.0.0.0',port=5000)
+    app.run(debug=True) #for development
