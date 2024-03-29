046ebbe023c8d6052fd0609d39b7e1a96e5c62d6
mr-verrma <ak.amy00@gmail.com>
2024-03-29 11:10:06 +0530

    added console for testing

--- Flask.py
+++ Flask.py
@@ -6,5 +6,15 @@ app=Flask(__name__)
 def hello():
     return 'Hello World!'
 
+
+console.log("hi")
+
+@app.route('/home',methods=['GET'])
+def get_products():
+    return "API FETCH CALL"
+
+@app.route('/home',methods=['POST'])
+def get_products():
+    return "API POST CALL"
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
