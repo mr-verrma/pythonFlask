d406db3f966330ac1ec0996cf654284067afeda1
mr-verrma <ak.amy00@gmail.com>
2024-04-06 23:21:14 +0530

    added del and remove method

--- dynamicListUsingCtypes.py
+++ dynamicListUsingCtypes.py
@@ -63,14 +63,29 @@ class DynamicList:
             
         return 'ValueError- Not Found'
 
-    def insert(self,pos,item):
+    def insert(self,index,item):
         if self.n==self.size:
             self.__resize(self.size*2)
-        for i in range(self.n,pos,-1):
+        for i in range(self.n,index,-1):
             self.List[i]=self.List[i-1]
-        self.List[pos]=item
+        self.List[index]=item
         self.n=self.n+1
 
+    def __delitem__(self,index):
+        if 0<= index <self.n:
+            for i in range(index,self.n-1):
+                self.List[i]=self.List[i+1]
+            self.n=self.n-1
+
+    def remove(self,item):
+        index=self.find(item)
+
+        if type(index) == int:
+            #delete
+            self.__delitem__(index)
+        else:
+            return index #Index Error
+        
 L=DynamicList()
 # print(type(L))
 L.append('abc')
@@ -78,5 +93,5 @@ L.append('abc2')
 L.append(True)
 L.append(2)
 
-L.insert(0,2)
+L.remove(2)
 print(L)
